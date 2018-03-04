f647834f49990756fd859d8185ca655f9716208d
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-04 18:42:01 +0100

    Add server, confugure HMR

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ if (env === 'production') {
 }
 
 module.exports = {
-	entry: './src/index.js',
+	entry: [
+		'react-hot-loader/patch',
+		'./src/index.js'
+	],
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'app.bundle.js'
